0386f95c7488974df319f81ba4838ae84dfdbb36
Sanjeev Jaiswal <sanjeevjaiswal2005@gmail.com>
2025-12-12 09:08:15 +0530

    Added new React concepts in 02-folder

---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>HI I am Sanjeev Jaiswal</h1>
+        <h2>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          debitis iure blanditiis?
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
